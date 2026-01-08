e7de682f423e172e35a471d60d9f4e938b8da7be
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-01-08 21:13:09 +0900

    Add song keys
    Happy Synthesizer / EasyPop, Key=F,167000,F#

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -111,5 +111,7 @@ const SONG_KEYS = new Map([
   ["staff.aist.go.jp/m.goto/RWC-MDB/MP3/RWC-MDB-P-2001.No.016.mp3", "Am"],
   ["staff.aist.go.jp/m.goto/RWC-MDB/MP3/RWC-MDB-P-2001.No.087.mp3", "F#"],
   ["staff.aist.go.jp/t.nakano/music/VocaWatcher.Prologue.Miku.mp3", "C#m"],
+  ["www.nicovideo.jp/watch/sm12825985", "F,167000,F#"],
+  ["www.youtube.com/watch?v=poiZSEjQBgw", "F,167000,F#"],
 ]);
 
