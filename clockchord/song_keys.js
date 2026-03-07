b0f7d95557288e4783ba5004feea0b6c612f5319
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-03-07 17:59:47 +0900

    Add song key
    Heart Of Glass / Blondie, Key=E

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -173,4 +173,6 @@ const SONG_KEYS = new Map([
   ["www.youtube.com/watch?v=1w7OgIMMRc4", "C#,153500,D#m"],
   ["www.youtube.com/watch?v=y6QBaZHltJw", "Dm,94500,Em,111500,Dm,131000,Em,144500,Dm"],
   ["www.nicovideo.jp/watch/sm4864513", "Dm,94500,Em,111500,Dm,131000,Em,144500,Dm"],
+  ["www.youtube.com/watch?v=WGU_4-5RaxU", "E"],
+  ["www.nicovideo.jp/watch/sm16608166", "E"],
 ]);
