f7cd8c1051b18652509e3a74c859a4381bf32686
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-03-14 11:13:10 +0900

    Add song key
    WHERE THE STREETS HAVE NO NAME / U2, Key=D

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -180,4 +180,5 @@ const SONG_KEYS = new Map([
   ["www.youtube.com/watch?v=POWsFzSFLCE", "Eb"],
   ["www.youtube.com/watch?v=4FAM-s3vSPY", "E"],
   ["www.nicovideo.jp/watch/sm1846945", "E"],
+  ["www.youtube.com/watch?v=WfunypXsBO4", "D"],
 ]);
