ca3cb4c13d341f8ca05b99d9ea01359bc117ed7f
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-03-31 20:25:18 +0900

    Add song key

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -198,4 +198,5 @@ const SONG_KEYS = new Map([
   ["www.youtube.com/watch?v=j44L958AJl8", "C"],
   ["www.youtube.com/watch?v=MKINc7Rs6v8", "C"],
   ["www.youtube.com/watch?v=Dsdxxrnz1AQ", "B,80000,Bm,102800,B,192000,Bm,215000,B"],
+  ["www.youtube.com/watch?v=ia0lAgfhbAo", "D"]
 ]);
