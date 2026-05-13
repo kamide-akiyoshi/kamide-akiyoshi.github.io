3165d5eec0e39f38bf3e9f17aceb94627fc41ebd
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-05-13 19:44:11 +0900

    Add @ts-check for WebMidiLink

--- clockchord/web_midi_link.js
+++ clockchord/web_midi_link.js
@@ -1,49 +1,53 @@
-
+// @ts-check
 /** @param {(data: number[]) => void} [onMessage] */
 const setupWebMidiLink = (onMessage) => {
   const ID_MIDI = "midi";
-  /** @param {MessageEvent<string>} event */
-  const midiMessageListener = (event) => {
-    const strs = event.data.split?.(",");
-    if( ! strs ) {
-      // Ignore non-string message from other source (such as Songle)
-      return;
-    }
-    const id = strs.shift();
-    switch(id) {
-      case ID_MIDI:
-        onMessage?.(strs.map((hexStr) => parseInt(hexStr, 16)));
-        break;
-    }
-  };
-  window.addEventListener('message', midiMessageListener);
-  /** @type {HTMLInputElement} */
-  const urlElement = document.getElementById('WebMidiLinkUrl');
-  /** @type {HTMLButtonElement} */
-  const loadButton = document.getElementById('LoadWebMidiLinkUrl');
-  /** @type {HTMLIFrameElement} */
-  const iFrame = document.getElementById('WebMidiLinkSynth');
-  if( urlElement && loadButton && iFrame ) {
-    const parent = iFrame.parentNode;
-    const attach = () => parent.contains(iFrame) || parent.appendChild(iFrame);
-    const detach = () => parent.contains(iFrame) && parent.removeChild(iFrame);
-    detach();
-    loadButton.addEventListener('click', () => {
-      const url = urlElement.value;
-      if( url ) {
-        attach();
-        iFrame.src = url;
-      } else {
-        iFrame.src = undefined;
-        detach();
+  //
+  // Add WebMidiLink message receiver
+  window.addEventListener(
+    'message',
+    (event) => {
+      const messageString = typeof event.data === "string" ? event.data : null;
+      if( ! messageString ) {
+        // Ignore non-string message from other source such as Songle
+        return;
       }
-    });
-    /** @param {number[]} data */
-    return (data) => {
-      iFrame.contentWindow?.postMessage(
-        data.reduce((str, num) => `${str},${(num).toString(16)}`, ID_MIDI),
-        "*"
-      );
-    };
-  }
+      const strs = messageString.split(",");
+      const id = strs.shift();
+      switch(id) {
+        case ID_MIDI:
+          onMessage?.(strs.map((hexStr) => parseInt(hexStr, 16)));
+          break;
+      }
+    }
+  );
+  //
+  // Return WebMidiLink message sender if available
+  const iFrame = document.getElementById('WebMidiLinkSynth');
+  if( !(iFrame instanceof HTMLIFrameElement) ) return;
+  const urlElement = document.getElementById('WebMidiLinkUrl');
+  if( !(urlElement instanceof HTMLInputElement) ) return;
+  const loadButton = document.getElementById('LoadWebMidiLinkUrl');
+  if( !(loadButton instanceof HTMLButtonElement) ) return;
+  const parent = iFrame.parentNode;
+  const attach = () => parent?.contains(iFrame) || parent?.appendChild(iFrame);
+  const detach = () => parent?.contains(iFrame) && parent.removeChild(iFrame);
+  detach();
+  loadButton.addEventListener('click', () => {
+    const url = urlElement.value;
+    if( url ) {
+      attach();
+      iFrame.src = url;
+    } else {
+      iFrame.src = "";
+      detach();
+    }
+  });
+  /** @param {number[]} data */
+  return (data) => {
+    iFrame.contentWindow?.postMessage(
+      data.reduce((str, num) => `${str},${(num).toString(16)}`, ID_MIDI),
+      "*"
+    );
+  };
 };
