c0ea07e727b7d2cd82ca5b84c1cf502db7dde629
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-05-25 21:06:06 +0900

    Add song key
    Beat It / Michael Jackson, Key=D#m

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -241,4 +241,5 @@ const SONG_KEYS = new Map([
   ["www.nicovideo.jp/watch/sm32021574", "E"],
   ["www.youtube.com/watch?v=wN2tHv8gPm8", "Em"],
   ["www.youtube.com/watch?v=ChwLT4ByNU8", "D,49900,C,99000,D,126500,C"],
+  ["www.youtube.com/watch?v=oRdxUFDoQe0", "D#m"],
 ]);
