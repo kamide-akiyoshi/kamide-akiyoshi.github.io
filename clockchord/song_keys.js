0578783bf2ffabcd087bab2fb583f4e16f4235f1
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-08-27 23:25:37 +0900

    Add song keys

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -160,4 +160,5 @@ const SONG_KEYS = new Map([
   ["www.nicovideo.jp/watch/sm18254466", "Am"],
   ["www.youtube.com/watch?v=vBmU5v2EyxM", "G#m,56500,Bm,72000,G#m,107000,Bm,122500,G#m,164500,Bm,180000,G#m"],
   ["www.nicovideo.jp/watch/sm42636421", "G,197000,E"],
+  ["www.nicovideo.jp/watch/nm33221779", "F,192500,F#"],
 ]);
