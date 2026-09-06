5bc66c1816fa23402b1c83508be1eddefaaf21b2
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-09-06 13:23:18 +0900

    Add song keys
    Everyday Is A Winding Road / Sheryl Crow, Key=C#,F#

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -165,4 +165,5 @@ const SONG_KEYS = new Map([
   ["www.nicovideo.jp/watch/sm4126687", "F#"],
   ["www.youtube.com/watch?v=J9gKyRmic20", "Eb"],
   ["www.youtube.com/watch?v=R53CceEY77Y", "Dm"],
+  ["www.youtube.com/watch?v=khrx-zrG460", "C#,60000,F#,77000,C#,119500,F#,153500,C#,198000,F#"],
 ]);
