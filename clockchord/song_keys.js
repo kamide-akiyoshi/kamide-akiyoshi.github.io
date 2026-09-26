e42fd104bd71a615c9b696251d595868d19a8319
Akiyoshi KAMIDE <kamide@yk.rim.or.jp>
2026-09-26 11:48:40 +0900

    Add song keys
    Hungly Like The Wolf / Duran Duran, Key=E,C

--- clockchord/song_keys.js
+++ clockchord/song_keys.js
@@ -125,4 +125,5 @@ const SONG_KEYS = new Map([
   ["www.nicovideo.jp/watch/sm31973439", "G"],
   ["www.youtube.com/watch?v=AYCu5mws-R0", "Em"],
   ["www.youtube.com/watch?v=yfZIaTZJo0o", "F,192000,F#,226000,F"],
+  ["www.nicovideo.jp/watch/sm6506463", "E,41400,C,71500,E,105500,C,136000,E,166000,C"],
 ]);
